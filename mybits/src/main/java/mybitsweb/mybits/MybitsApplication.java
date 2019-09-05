c0270b696d8df70ed70df2732b58187df17bd2e0
zhuoyuec <zhuoyuec@yonyou.com>
2019-09-05 19:29:13 +0800

    mybits模块添加

---
+++ mybits/src/main/java/mybitsweb/mybits/MybitsApplication.java
@@ -0,0 +1,13 @@
+package mybitsweb.mybits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybitsApplication.class, args);
+    }
+
+}
---
+++ mybits/src/test/java/mybitsweb/mybits/MybitsApplicationTests.java
@@ -0,0 +1,16 @@
+package mybitsweb.mybits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybitsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
